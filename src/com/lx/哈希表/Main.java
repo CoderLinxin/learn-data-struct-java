6effb90140c1b5beaeeeda81e1cd0d0db752d2e4
linxin <linxin@qq.com>
2022-03-17 01:31:35 +0800

    哈希表实现

--- src/com/lx/哈希表/Main.java
+++ src/com/lx/哈希表/Main.java
@@ -1,9 +1,15 @@
 package com.lx.哈希表;
 
 import com.lx.哈希表.model.Key;
+import com.lx.哈希表.model.SubKey1;
+import com.lx.哈希表.model.SubKey2;
 import com.lx.哈希表.src.HashMap;
+import com.lx.哈希表.src.LinkedHashMap;
+import com.lx.映射.src.Map.Visitor;
+import com.lx.哈希表.test.Asserts;
 import com.lx.映射.src.Map;
 
+import java.util.Date;
 import java.util.Objects;
 
 public class Main {
@@ -127,6 +133,117 @@ public class Main {
         }
     }
 
+    static void test3(HashMap<Object, Integer> map) {
+        for (int i = 1; i <= 20; i++) {
+            map.put(new Key(i), i);
+        }
+        for (int i = 5; i <= 7; i++) {
+            map.put(new Key(i), i + 5);
+        }
+        Asserts.test(map.size() == 20);
+        Asserts.test(map.get(new Key(4)) == 4);
+        Asserts.test(map.get(new Key(5)) == 10);
+        Asserts.test(map.get(new Key(6)) == 11);
+        Asserts.test(map.get(new Key(7)) == 12);
+        Asserts.test(map.get(new Key(8)) == 8);
+    }
+
+    static void test4(HashMap<Object, Integer> map) {
+        map.put(null, 1); // 1
+        map.put(new Object(), 2); // 2
+        map.put("jack", 3); // 3
+        map.put(10, 4); // 4
+        map.put(new Object(), 5); // 5
+        map.put("jack", 6);
+        map.put(10, 7);
+        map.put(null, 8);
+        map.put(10, null);
+        Asserts.test(map.size() == 5);
+        Asserts.test(map.get(null) == 8);
+        Asserts.test(map.get("jack") == 6);
+        Asserts.test(map.get(10) == null);
+        Asserts.test(map.get(new Object()) == null);
+        Asserts.test(map.containsKey(10));
+        Asserts.test(map.containsKey(null));
+        Asserts.test(map.containsValue(null));
+        Asserts.test(map.containsValue(1) == false);
+    }
+
+    static void test5(HashMap<Object, Integer> map) {
+        map.put("jack", 1);
+        map.put("rose", 2);
+        map.put("jim", 3);
+        map.put("jake", 4);
+        map.remove("jack");
+        map.remove("jim");
+        for (int i = 1; i <= 10; i++) {
+            map.put("test" + i, i);
+            map.put(new Key(i), i);
+        }
+        for (int i = 5; i <= 7; i++) {
+            Asserts.test(map.remove(new Key(i)) == i);
+        }
+        for (int i = 1; i <= 3; i++) {
+            map.put(new Key(i), i + 5);
+        }
+        Asserts.test(map.size() == 19);
+        Asserts.test(map.get(new Key(1)) == 6);
+        Asserts.test(map.get(new Key(2)) == 7);
+        Asserts.test(map.get(new Key(3)) == 8);
+        Asserts.test(map.get(new Key(4)) == 4);
+        Asserts.test(map.get(new Key(5)) == null);
+        Asserts.test(map.get(new Key(6)) == null);
+        Asserts.test(map.get(new Key(7)) == null);
+        Asserts.test(map.get(new Key(8)) == 8);
+        map.traversal(new Visitor<Object, Integer>() {
+            public boolean visit(Object key, Integer value) {
+                System.out.println(key + "_" + value);
+                return false;
+            }
+        });
+    }
+
+    static void test6(HashMap<Object, Integer> map) {
+        for (int i = 1; i <= 20; i++) {
+            map.put(new SubKey1(i), i);
+        }
+        map.put(new SubKey2(1), 5);
+        Asserts.test(map.get(new SubKey1(1)) == 5);
+        Asserts.test(map.get(new SubKey2(1)) == 5);
+        Asserts.test(map.size() == 20);
+    }
+
+    static void test7(HashMap<Object, Integer> map, int count) {
+        double[] datas = new double[count];
+        for (int i = 0; i < count; i++)
+            datas[i] = (Math.random() - 0.5) * 1000;
+
+        for (int i = 0; i < count; i++)
+            map.put(new SubKey1((int) datas[i]), i);
+        for (int i = 0; i < count; i++)
+            map.containsKey(new SubKey1((int) datas[i]));
+        for (int i = 0; i < count; i++)
+            map.containsValue((int) datas[i]);
+        for (int i = 0; i < count; i++)
+            map.remove(new SubKey1((int) datas[i]));
+    }
+
+    static void test8(LinkedHashMap<Object, Integer> map) {
+        for (int i = 0; i < 20; i++)
+            map.put(new SubKey1(i), i);
+
+        map.remove(new SubKey1(10));
+        System.out.println(map.size());
+
+        map.traversal(new Visitor<Object, Integer>() {
+            @Override
+            public boolean visit(Object key, Integer value) {
+                System.out.println("K_" + key + "V_" + value);
+                return false;
+            }
+        });
+    }
+
     public static void main(String[] args) {
 //        getHash();
 //        getHashWithObj();
@@ -134,6 +251,17 @@ public class Main {
 //        System.out.println(2 << 4);
 //        test1();
 
-        test2();
+//        test2();
+
+//        long start = new Date().getTime();
+//        test3(new HashMap<>());
+//        test4(new HashMap<>());
+//        test5(new HashMap<>());
+//        test6(new HashMap<>());
+////        test7(new HashMap<>(), 500000);
+//        long end = new Date().getTime();
+//        System.out.println((end - start) / 1000.0 + "s");
+
+        test8(new LinkedHashMap<>());
     }
 }
--- src/com/lx/哈希表/src/HashMap.java
+++ src/com/lx/哈希表/src/HashMap.java
@@ -18,6 +18,7 @@ public class HashMap<K, V> implements Map<K, V> {
     private static final int DEFAULT_CAPACITY = 1 << 4; // 数组默认容量(2^n，这里取 4)
     private Node<K, V>[] table; // 承载哈希表的数组(每个数组元素是一棵红黑树的根结点)
     private int size; // 哈希表总元素个数(包括所有冲突和非冲突的元素)
+    private static final float LOADING_FACTOR = 0.75f; // 哈希表扩容时的装填因子
 
     public HashMap() {
         this.table = new Node[DEFAULT_CAPACITY];
@@ -56,30 +57,35 @@ public class HashMap<K, V> implements Map<K, V> {
      * 向哈希表中添加映射
      * 哈希表存储的结点(这里存储的是映射)不具备可比较性，但是由于使用的是红黑树实现哈希表。
      * 红黑树插入，查找和删除结点时根据的是结点的可比较性
-     * (因此这里我们通过任何可比较的途径(因为这样能够充分发挥二叉搜索树的性能)尝试是否能够成功地添加、查找、删除,
-     * 最后实在不得已的比较策略是,选择计算内存地址差值的方式来得到插入结点的左右子树方向,
-     * 而以后在(哈希表没有扩容前)红黑树上搜索该结点时都需要通过扫描的方式才能搜索得到)
+     * 因此我们在查找过程中通过任何可比较的途径(制定一系列比较规则)(因为这样能够充分发挥二叉搜索树的性能)
+     * 来确定目标结点的方向(如何继续走下去)(否则一上来就直接扫描会浪费很多性能),而通过 equals 是否等于 0，判断是否是目标结点
+     * 最后实在不得已(连扫描都扫描不到时)的比较策略是,选择计算内存地址差值的方式来得到插入结点的左右子树方向
+     * (这样至少有依据能够添加该结点到红黑树上，其实只要能挂到树上就行了，例如让 cmp 一直为 1，一直往右放也行，
+     * 甚至随便搞个不等于 0 的随机数都行，因为 findNode 搜索时也不会按照最后的这条规则走(条件太苛刻)，而是会进行扫描。
+     * 但还是建议按照一定的规则确定方向(有时比较好调试。))
      *
      * @param k1    添加映射的键
      * @param value 值
      * @return 旧映射的值(旧映射不存在则返回 null)
      */
     public V put(K k1, V value) {
+        this.resize(); // 添加结点前判断是否需要扩容
+
         int index = this.getIndex(k1); // 计算 key 对应的索引
 
         Node<K, V> root = this.table[index]; // index 对应的 table 中的红黑树根结点
 
         if (root == null) { // 没有产生哈希冲突
             // 种一棵红黑树(这里添加的是红黑树的根结点，因为红黑树对象多出来的一些成员变量对于哈希表并没有用)
-            table[index] = root = new Node<>(k1, value, null);
+            table[index] = root = this.createNode(k1, value, null);
             this.size++;
-            afterPut(root); // 修复红黑树性质
+            fixAfterPut(root); // 修复红黑树性质
             return null;
         }
 
         /* 产生哈希冲突(需要查找红黑树，并将映射的结点挂载/更新到红黑树上) */
 
-        int h1 = k1.hashCode();
+        int h1 = this.getHash(k1);
         int cmp = 0; // 存储比较结果
         Node<K, V> node = root;
         Node<K, V> parent = root; // 记录遍历过程的父节点
@@ -92,18 +98,18 @@ public class HashMap<K, V> implements Map<K, V> {
             k2 = node.key;
             h2 = node.hash;
 
-            /* 先比较哈希值 */
+            /* 先比较哈希值(确定方向) */
             if (h1 > h2) cmp = 1;
             else if (h1 < h2) cmp = -1;
 
-                /* 哈希值相同则判断是否 equals */
+                /* 哈希值相同(无法确定接下来的方向)则判断是否 equals */
             else if (Objects.equals(k1, k2)) { // 相等则更新结点
                 cmp = 0;
-            } else if (k1 != null && k2 != null // 是否为可比较对象
+            } else if (k1 != null && k2 != null // 是否为可比较对象(第二种方式确定方向)
                     && k1.getClass() == k2.getClass()
-                    && k1 instanceof Comparable) {
-                cmp = ((Comparable) k1).compareTo(k2);
-            } else if (searched) { // 扫描完整棵树后仍未发现目标结点说明需要插入新结点，接下来一直比较内存地址即可
+                    && k1 instanceof Comparable
+                    && (cmp = ((Comparable) k1).compareTo(k2)) != 0) { // compareTo 的结果为 0 说明无法确定方向
+            } else if (searched) { // 扫描完整棵树后仍未发现目标结点说明需要插入新结点，接下来一直前往内存地址差值得出的方向即可
                 cmp = System.identityHashCode(k1) - System.identityHashCode(k2);
             } else {
                 Node<K, V> target = null;
@@ -111,8 +117,8 @@ public class HashMap<K, V> implements Map<K, V> {
                 if (node.left != null && (target = this.findNode(node.left, k1)) != null
                         || node.right != null && (target = this.findNode(node.right, k1)) != null) {
                     node = target; // 发现存在 key 对应的结点
-                    cmp = 0;
-                } else { // 扫描失败，红黑树上找不到该结点，需要插入新结点
+                    cmp = 0; // 标记为此次操作为覆盖操作
+                } else { // 扫描失败，红黑树上找不到该结点，表明本次操作是插入新结点
                     // 通过内存地址计算插入结点的左右子树方向(通过这种方式插入的结点以后一般都需要通过扫描的方式才能查找到(在没有重新散列的情况下))
                     // 内存地址都是正的，相减必然不会溢出
                     cmp = System.identityHashCode(k1) - System.identityHashCode(k2);
@@ -132,17 +138,127 @@ public class HashMap<K, V> implements Map<K, V> {
             }
         }
 
-        // 插入新结点
-        Node<K, V> newNode = new Node<>(k1, value, parent);
+        // 插入新结点的情况
+        Node<K, V> newNode = this.createNode(k1, value, parent);
         if (cmp < 0) parent.left = newNode;
         else parent.right = newNode;
         this.size++;
 
-        afterPut(newNode); // 修复红黑树性质
+        fixAfterPut(newNode); // 修复红黑树性质
         return null;
     }
 
-    private void afterPut(Node<K, V> node) { // 添加映射后修复红黑树性质
+
+    /**
+     * 创建结点(可被子类重写)
+     *
+     * @param key    键
+     * @param value  值
+     * @param parent 父结点指针
+     * @return 创建的结点
+     */
+    protected Node<K, V> createNode(K key, V value, Node<K, V> parent) {
+        return new Node<>(key, value, parent);
+    }
+
+    private void resize() {
+        if (((this.size + 0.0) / this.table.length) <= LOADING_FACTOR) return; // 判断是否符合扩容界限
+
+        Node<K, V>[] oldTable = this.table; // 保存旧的 table 准备搬迁
+        this.table = new Node[this.table.length << 1]; // 对 table 进行扩容
+
+        Queue<Node<K, V>> queue = new LinkedList<>();
+        Node<K, V> node;
+
+        // 遍历整个 table
+        for (int i = 0; i < oldTable.length; i++) {
+            if (oldTable[i] == null) continue;
+            queue.offer(oldTable[i]);
+
+            // 每个桶的红黑树搬家
+            while (!queue.isEmpty()) {
+                node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+
+                this.moveNode(node); // 每个结点搬家
+            }
+        }
+    }
+
+    /**
+     * 将传递进来的旧哈希表中的结点移动到当前新的 table 中
+     *
+     * @param node 待移动的结点
+     */
+    private void moveNode(Node<K, V> node) {
+        this.reset(node); // 重置结点
+        int index = this.getIndex(node); // 重新计算索引
+        Node<K, V> root = this.table[index]; // 获取对应桶中的红黑树根结点
+
+        /* 未产生哈希冲突 */
+
+        if (root == null) {
+            table[index] = node; // 直接成为根结点
+            fixAfterPut(node); // 修复红黑树性质
+            return;
+        }
+
+        /* 产生哈希冲突 */
+
+        K k1 = node.key;
+        int h1 = node.hash;
+        K k2;
+        int h2;
+        int cmp = 0; // 存储比较结果
+        Node<K, V> traverNode = root; // 用于遍历的结点
+        Node<K, V> parent = root; // 记录遍历过程的父节点
+
+        // 由于新 table 必不可能存在旧 table 中还未搬迁过来的结点(equals 必不等于 true，因此不必考虑 equals 的情况)
+        while (traverNode != null) {
+            parent = traverNode;
+            k2 = traverNode.key;
+            h2 = traverNode.hash;
+
+            // 比较哈希值(确定方向)
+            if (h1 > h2) cmp = 1;
+            else if (h1 < h2) cmp = -1;
+            else if (k1 != null && k2 != null
+                    && k1 instanceof Comparable
+                    && k1.getClass() == k2.getClass()
+                    && (cmp = ((Comparable) k1).compareTo(k2)) != 0) ;
+            else cmp = System.identityHashCode(k1) - System.identityHashCode(k2);
+
+            // 继续向下查找
+            if (cmp > 0) traverNode = traverNode.right;
+            else traverNode = traverNode.left;
+        }
+
+        // 插入结点
+        if (cmp < 0) parent.left = node;
+        else parent.right = node;
+        node.parent = parent;
+        fixAfterPut(node); // 修复红黑树性质
+    }
+
+    /**
+     * 对结点进行重置
+     *
+     * @param node 待重置的结点
+     */
+    private void reset(Node<K, V> node) {
+        node.left = null;
+        node.parent = null;
+        node.right = null;
+        node.color = RED;
+    }
+
+    /**
+     * 添加结点后修复红黑树性质
+     *
+     * @param node 添加的结点
+     */
+    private void fixAfterPut(Node<K, V> node) { // 添加映射后修复红黑树性质
         Node<K, V> parent = node.parent;
 
         // 添加的是根节点 或者 上溢到达了根节点
@@ -162,7 +278,7 @@ public class HashMap<K, V> implements Map<K, V> {
             black(parent);
             black(uncle);
             // 把祖父节点当做是新添加的节点
-            afterPut(grand);
+            fixAfterPut(grand);
             return;
         }
 
@@ -251,16 +367,25 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     /**
-     * 根据 key 计算 index
+     * 对 key 的 hash 进行扰动计算
+     *
+     * @param key 键
+     * @return 扰动计算后的 hash
+     */
+    private int getHash(K key) {
+        if (key == null) return 0; // key 为 null 的映射统一放到索引为 0 处
+        int hash = key.hashCode();
+        return (hash ^ (hash >>> 16)); // 重新对高 16 位和低 16 位混合计算哈希值(高16位与 00..00 异或结果仍为高16位原本数值不变)
+    }
+
+    /**
+     * 根据 key 的哈希值(经过了扰动计算)计算出索引
      *
      * @param key 键
      * @return 索引
      */
     private int getIndex(K key) {
-        if (key == null) return 0; // key 为 null 的映射统一放到索引为 0 处
-        int hash = key.hashCode();
-        int hashCode = (hash ^ (hash >>> 16)); // 重新对高 16 位和低 16 位混合计算哈希值(高16位与 00..00 异或结果仍为高16位原本数值不变)
-        return hashCode & (this.table.length - 1); // 根据哈希值计算得到索引
+        return getHash(key) & (this.table.length - 1);
     }
 
     /**
@@ -270,7 +395,7 @@ public class HashMap<K, V> implements Map<K, V> {
      * @return 索引
      */
     private int getIndex(Node<K, V> node) {
-        return (node.hash ^ (node.hash >>> 16)) & (this.table.length - 1);
+        return node.hash & (this.table.length - 1); // 根据哈希值计算得到索引
     }
 
     /**
@@ -307,11 +432,12 @@ public class HashMap<K, V> implements Map<K, V> {
         size--;
 
         V oldValue = node.value;
-
+        Node<K, V> willRemoveNode = node;
         if (node.hasTwoChildren()) { // 度为2的节点
             // 找到后继节点
             Node<K, V> s = successor(node);
             // 用后继节点的值覆盖度为2的节点的值
+            node.hash = s.hash;
             node.key = s.key;
             node.value = s.value;
             // 删除后继节点
@@ -334,7 +460,7 @@ public class HashMap<K, V> implements Map<K, V> {
             }
 
             // 删除节点之后的处理
-            afterRemove(replacement);
+            fixAfterRemove(replacement);
         } else if (node.parent == null) { // node是叶子节点并且是根节点
             this.table[index] = null; // 更新红黑树的根结点
         } else { // node是叶子节点，但不是根节点
@@ -344,8 +470,8 @@ public class HashMap<K, V> implements Map<K, V> {
                 node.parent.right = null;
             }
 
-            // 删除节点之后的处理
-            afterRemove(node);
+            fixAfterRemove(node); // 删除结点后修复红黑树性质
+            afterRemove(willRemoveNode, node); // 删除结点后的一些处理
         }
 
         return oldValue;
@@ -371,7 +497,21 @@ public class HashMap<K, V> implements Map<K, V> {
         return node.parent;
     }
 
-    private void afterRemove(Node<K, V> node) {
+    /**
+     * 删除结点后的一些处理
+     *
+     * @param willRemoveNode 想要删除的结点
+     * @param removeNode     实际被删除的结点
+     */
+    protected void afterRemove(Node<K, V> willRemoveNode, Node<K, V> removeNode) {
+    }
+
+    /**
+     * 删除结点后修复红黑树性质
+     *
+     * @param node 实际被删除的结点或替代结点
+     */
+    private void fixAfterRemove(Node<K, V> node) {
         // 如果删除的节点是红色
         // 或者 用以取代删除节点的子节点是红色
         if (isRed(node)) {
@@ -402,7 +542,7 @@ public class HashMap<K, V> implements Map<K, V> {
                 black(parent);
                 red(sibling);
                 if (parentBlack) {
-                    afterRemove(parent);
+                    fixAfterRemove(parent);
                 }
             } else { // 兄弟节点至少有1个红色子节点，向兄弟节点借元素
                 // 兄弟节点的左边是黑色，兄弟要先旋转
@@ -432,7 +572,7 @@ public class HashMap<K, V> implements Map<K, V> {
                 black(parent);
                 red(sibling);
                 if (parentBlack) {
-                    afterRemove(parent);
+                    fixAfterRemove(parent);
                 }
             } else { // 兄弟节点至少有1个红色子节点，向兄弟节点借元素
                 // 兄弟节点的左边是黑色，兄弟要先旋转
@@ -531,31 +671,29 @@ public class HashMap<K, V> implements Map<K, V> {
      * @return 查找到的目标结点
      */
     private Node<K, V> findNode(Node<K, V> node, K k1) {
-        int h1 = k1.hashCode();
+        int h1 = this.getHash(k1); // null 的哈希值默认设为 0
 
         // 开始查找
         K k2;
         int h2;
+        int cmp;
         while (node != null) {
             k2 = node.key;
             h2 = node.hash;
 
-            /* 比较哈希值(不要使用减法，因为可能回溢出):哈希值不同,对应的 key 必然不同(想想逆否命题)，直接跳过当前结点继续向下遍历 */
+            /* 比较哈希值(不要使用减法，因为可能会溢出):哈希值不同,对应的 key 必然不同(想想逆否命题)，直接跳过当前结点继续向下遍历 */
             if (h1 > h2) node = node.right;
             else if (h1 < h2) node = node.left;
 
                 /* 哈希值相等(key 仍然可能不同)则判断是否 equals */
             else if (Objects.equals(k2, k1)) return node;
 
-                /* 是否为可比较对象 */
+                /* 是否为可比较对象:继续试探方向 */
             else if (k2 != null && k1 != null
                     && k2.getClass() == k1.getClass()
-                    && k2 instanceof Comparable) {
-                int cmp = ((Comparable) k2).compareTo(k1);
-
-                if (cmp > 0) node = node.right;
-                else if (cmp < 0) node = node.left;
-                else return node;
+                    && k2 instanceof Comparable
+                    && (cmp = ((Comparable) k2).compareTo(k1)) != 0) { // compareTo 为 0 说明无法确定方向
+                node = cmp > 0 ? node.right : node.left;
             } else {
                 /* 哈希值相等
                 k1 k2 类型不同
@@ -579,13 +717,13 @@ public class HashMap<K, V> implements Map<K, V> {
      * @param <K> 可比较的键
      * @param <V> 值
      */
-    private static class Node<K, V> {
+    protected static class Node<K, V> {
         // key 对应的 hashCode (放在这里做到一个缓存的作用，不用每次比较 key 旧计算一个 hashCode)
         // 只要映射第一次被添加到哈希表上(生成结点)后，hashCode 就会被缓存起来
         int hash;
         K key;
         V value;
-        boolean color = RED;
+        boolean color = RED; // 默认添加结点为红色
         Node<K, V> left;
         Node<K, V> right;
         Node<K, V> parent;
@@ -594,7 +732,8 @@ public class HashMap<K, V> implements Map<K, V> {
             this.key = key;
             this.value = value;
             this.parent = parent;
-            this.hash = key == null ? 0 : key.hashCode(); // key 为 null，哈希值默认设为 0
+            int hashCode = key == null ? 0 : key.hashCode(); // key 为 null，哈希值固定设为 0
+            this.hash = hashCode ^ (hashCode >>> 16); // 进行扰动计算后并缓存(高低16位混合运算)
         }
 
         public boolean isLeaf() {
